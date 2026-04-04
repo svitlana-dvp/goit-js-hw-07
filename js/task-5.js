05b0ebecf0734bb6ea0992d6f1c6618e14672640
Svitlana <lanamartinenko180@gmail.com>
2026-04-04 14:23:01 +0300

    save

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+const spanClrEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", (event) => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanClrEl.textContent = randomColor;
+});
